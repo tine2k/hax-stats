3158b54a1bd6a2566145385e6720dfb5ddbed8df
Martin Maier-Moessner <martin.maier.moessner@gmail.com>
2021-04-10 21:02:14 +0200

    add cors

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
+
+const corsOptions = {
+    origin: 'https://www.haxball.com',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 app.use(express.json());
 app.use(express.static('public'));
 
-app.post('/addStats', (req, res) => {
-  console.log(req.body);
-  res.send('OK');
+app.options('/addStats', cors());
+
+app.post('/addStats', cors(corsOptions), (req, res) => {
+    console.log(req.body);
+    res.send('OK');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`);
 });
